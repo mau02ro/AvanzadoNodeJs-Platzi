1b1fc3058a4341a1bcaf065879686451d86d65f7
Mauro Hernández <mauroDeveloper@hotmail.com>
2020-12-19 00:31:07 -0400

    Implementacion del modulo de bases de datos

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,21 @@
 'use sctrict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  // Definir las relaciones entre modelos
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  // Validar la configuracionde la base de datos
+  await sequelize.authenticate()
+
   const Agent = {}
   const Metric = {}
 
