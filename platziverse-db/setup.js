f88cacc4851df6f6b66f42f5884347713434bbdd
Mauro Hernández <mauroDeveloper@hotmail.com>
2020-12-19 03:51:12 -0400

    Configuración del prompt de pregunta

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,12 +1,30 @@
 'use strict'
+
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happene :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
-    password: process.env.DB_PASS || 'platzi',
+    password: process.env.DB_PASS || 'platzi8',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: (s) => debug(s),
@@ -19,7 +37,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}}`)
   console.error(err.stack)
   process.exit(1)
 }
