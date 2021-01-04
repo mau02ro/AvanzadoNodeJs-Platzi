e3275b107f03ea66177e3d2ac706664dbfddb692
Mauro Hernández <mauroDeveloper@hotmail.com>
2021-01-04 02:36:06 -0400

    Clase 19

--- platziverse-db/tests/agente-tests.js
+++ platziverse-db/tests/agente-tests.js
@@ -1,17 +1,47 @@
-'use strict'
+"use strict";
+const proxyquire = require("proxyquire");
+const test = require("ava");
+const sinon = require("sinon");
 
-const test = require('ava')
 const config = {
-  logging: function () {}
-}
-let db = null
+  logging: function () {},
+};
+
+let MetricStub = {
+  belongsTo: sinon.spy(), // un spy es una funcion específica de sinon que se usa al momento de hacer pruebas
+};
+
+let AgentStub = null;
+let db = null;
+let sandbox = null;
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  /* un sanbox es un ambiente específico de sinon que solo va a funccionar
+  para un caso en particular cuando termine la prueba se reiniciara.*/
+  sandbox = sinon.createSandbox();
 
-  db = await setupDatabase(config)
-})
+  AgentStub = {
+    hasMany: sandbox.spy(),
+  };
 
-test('Agent', (t) => {
-  t.truthy(db.Agent, 'Agent service should exist')
-})
+  /*redefinimos las rutas para los test */
+  const setupDatabase = proxyquire("../", {
+    "./models/agent": () => AgentStub,
+    "./models/metric": () => MetricStub,
+  });
+
+  db = await setupDatabase(config);
+});
+
+test.after(() => {
+  sandbox && sinon.reset();
+});
+
+test("Agent", (t) => {
+  t.truthy(db.Agent, "Agent service should exist");
+});
+
+test.serial("Setup", (t) => {
+  t.true(AgentStub.hasMany.called, "AgentModel.hasMany was executed");
+  t.true(MetricStub.belongsTo.called, "MetricStub.belongsTo was executed");
+});
