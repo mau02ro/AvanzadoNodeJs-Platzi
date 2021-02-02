333d61ea907685a5d48246cd38a3441e081de4ec
Mauro Hernandez <mauroDeveloper@hotmail.com>
2021-02-02 18:29:28 -0400

    Todo ok

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,21 +1,22 @@
-'use strict';
+'use strict'
 
-const debug = require('debug')('platziverse:mqtt');
-const mosca = require('mosca');
-const redis = require('redis');
-const chalk = require('chalk');
-const db = require('platziverse-db');
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+const db = require('platziverse-db')
+const { parserPayload } = require('./utils')
 
 const backend = {
   type: 'redis',
   redis,
-  return_buffers: true,
-};
+  return_buffers: true
+}
 
 const settings = {
   port: 1883,
-  backend: backend,
-};
+  backend: backend
+}
 
 const db_config = {
   database: process.env.DB_NAME || 'platziverse',
@@ -23,42 +24,129 @@ const db_config = {
   password: process.env.DB_PASS || 'platzi',
   host: process.env.DB_HOST || 'localhost',
   dialect: 'postgres',
-  logging: (s) => debug(s),
-};
+  logging: (s) => debug(s)
+}
 
-const server = new mosca.Server(settings);
+const server = new mosca.Server(settings)
 
-let Agent, Metric;
+const clients = new Map()
+
+let Agent, Metric
 
 server.on('clientConnected', (client) => {
-  debug(`Client Connected: ${client.id}`);
-});
+  debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
+})
 
-server.on('clientDisconnected', (client) => {
-  debug(`Client Disconnected: ${client.id}`);
-});
+server.on('clientDisconnected', async (client) => {
+  debug(`Client Disconnected: ${client.id}`)
+  const agent = clients.get(client.id)
+  if (agent) {
+    // Mark agent as Disconnected
+    agent.connected = false
 
-server.on('published', (packet, client) => {
-  debug(`Received: ${packet.topic}`); // topic es el tipo del mensaje
-  debug(`Payload: ${packet.payload}`); //
-});
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (error) {
+      return handleError(error)
+    }
+
+    // Delete agent from clients list
+    clients.delete(client.id)
+
+    server.publish({
+      topic: 'Disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid
+        }
+      })
+    })
+    debug(
+      `Client (${client.id}) associated to agent (${agent.uuid}) marked as Disconnected`
+    )
+  }
+})
+
+server.on('published', async (packet, client) => {
+  debug(`Received: ${packet.topic}`) // topic es el tipo del mensaje
+
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+
+    case 'agent/message':
+      const payload = parserPayload(packet.payload)
+      if (payload) {
+        debug(`Payload: ${packet.payload}`)
+
+        payload.agent.connected = true
+
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (error) {
+          return handleError(error)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+
+        // Store Metrics
+        for (const metric of payload.metrics) {
+          let m
+
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (error) {
+            return handleError(error)
+          }
+
+          debug(`Metric ${m.id} saved on agent ${agent.uuid}`)
+        }
+      }
+      break
+  }
+})
 
 server.on('ready', async () => {
-  //Instanciar la db
-  const servicies = await db(db_config).catch(handleFatalError);
+  // Instanciar la db
+  const servicies = await db(db_config).catch(handleFatalError)
 
-  Agent = servicies.Agent;
-  Metric = servicies.Metric;
+  Agent = servicies.Agent
+  Metric = servicies.Metric
 
-  console.log(`${chalk.green('[platziverse-mqtt]')} - Server is running`);
-});
+  console.log(`${chalk.green('[platziverse-mqtt]')} - Server is running`)
+})
 
-server.on('error', handleFatalError);
-process.on('uncaughtException', handleFatalError);
-process.on('unhandledRejection', handleFatalError);
+server.on('error', handleFatalError)
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
-function handleFatalError(err) {
-  console.error(`${chalk.red('[Fatal error] -> ')} ${err.message}`);
-  console.error(err.stack);
-  process.exit(1);
+function handleFatalError (err) {
+  console.error(`${chalk.red('[Fatal error] -> ')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.red('[Error] -> ')} ${err.message}`)
+  console.error(err.stack)
 }
